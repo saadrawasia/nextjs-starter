8d7b4601b8d4d5a9fc4daa44be5101e41642ec53
Saad Rawasia <saad.rawasia15@gmail.com>
2025-03-22 20:46:45 +0100

    Setup HeroUI

---
+++ src/components/providers.tsx
@@ -0,0 +1,7 @@
+import { ReactNode } from 'react'
+
+import { HeroUIProvider } from '@heroui/react'
+
+export default function Providers({ children }: { children: ReactNode }) {
+  return <HeroUIProvider>{children}</HeroUIProvider>
+}
