b89a580af5781448efa260b774c63290471a39df
Saad Rawasia <saad.rawasia15@gmail.com>
2025-03-23 12:38:59 +0100

    Added Next.js routing

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -1,7 +1,14 @@
+'use client'
+
+import { useRouter } from 'next/navigation'
 import { ReactNode } from 'react'
 
 import { HeroUIProvider } from '@heroui/react'
 
 export default function Providers({ children }: { children: ReactNode }) {
-  return <HeroUIProvider>{children}</HeroUIProvider>
+  const router = useRouter()
+
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-expect-error
+  return <HeroUIProvider navigate={router.push}>{children}</HeroUIProvider>
 }
